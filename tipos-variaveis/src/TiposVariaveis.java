8a111d3e7e2f1dbce5ad182d9fe370b748867deb
Lucas Coutinho <CucasLoutinho@gmail.com>
2025-04-03 23:39:00 -0300

    Documentando

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Correção na definição da variável String
+        String nome = "Lucas";
+        
+        // Exemplo de conversão de tipos
+        double numeroCurto = 1; // Variável double
+        int numeroNormal = (int) numeroCurto; // Conversão explícita de double para int
+        short numeroCurto2 = (short) numeroNormal; // Conversão explícita de int para short
+        
+        // Modificação do valor de uma variável inteira
+        int numero = 5;
+        numero = 10; // Valor atualizado
+        
+        // Imprimindo o valor atualizado no console
+        System.out.println(numero);
+        
+        // Definindo uma constante com a palavra-chave 'final'
+        final double VALOR_DE_PI = 3.14;
+
+        // Imprimindo o valor da constante
+        System.out.println("Valor de PI: " + VALOR_DE_PI);
     }
 }
